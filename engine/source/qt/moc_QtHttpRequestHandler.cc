87f3a56693157fadf234956d35cc999f5851d1fa
KyleNMiller-GG <kylem@garagegames.com>
2012-12-07 14:46:10 -0800

    Testing ignoring of Qt moc files

--- engine/source/qt/moc_QtHttpRequestHandler.cc
+++ engine/source/qt/moc_QtHttpRequestHandler.cc
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'QtHttpRequestHandler.h'
 **
-** Created: Fri Dec 7 14:35:30 2012
+** Created: Fri Dec 7 14:43:31 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
